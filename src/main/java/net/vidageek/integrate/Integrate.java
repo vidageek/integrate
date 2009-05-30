7c3c1cb3115354ba4e149b6330203ee112b1be2e
Jonas Abreu <jonas@vidageek.net>
2009-05-30 01:57:44 -0300

    cleanning up and adding filesystem as dependencie

--- src/main/java/net/vidageek/integrate/Integrate.java
+++ src/main/java/net/vidageek/integrate/Integrate.java
@@ -1,15 +1,8 @@
 package net.vidageek.integrate;
 
-import java.io.File;
-
-import net.vidageek.integrate.scm.Repository;
-import net.vidageek.integrate.scm.User;
 
 final public class Integrate {
     public static void main(final String[] args) throws Exception {
-        Project project = new Project(new Name(""), new Repository("",
-                new User(new Name("nome"), new Password("senha"))));
 
-        new ContinuousIntegrationManager(new File("rootDir")).add(project);
     }
 }
